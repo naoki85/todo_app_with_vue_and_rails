217e213aa067494f4d3107727d3bd274df34a346
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-09-14 18:43:09 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/about',   to: 'home#index'
+  get '/contact', to: 'home#index'
 
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
